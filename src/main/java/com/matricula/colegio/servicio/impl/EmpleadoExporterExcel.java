380e4ea77f5fcee5af1ac3e43c35159da72ff871
emerson <emersonsj785@gmail.com>
2023-11-16 00:00:07 -0500

    fix: Exportacion Empleados - Datos

--- src/main/java/com/matricula/colegio/servicio/impl/EmpleadoExporterExcel.java
+++ src/main/java/com/matricula/colegio/servicio/impl/EmpleadoExporterExcel.java
@@ -1,12 +1,11 @@
 package com.matricula.colegio.servicio.impl;
-
 import java.io.IOException;
 import java.util.List;
-
 import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.util.CellRangeAddress;
@@ -15,6 +14,7 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import com.matricula.colegio.entidad.Empleado;
+import com.matricula.colegio.entidad.Usuario;
 
 import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletResponse;
@@ -45,10 +45,12 @@ public class EmpleadoExporterExcel
 	    estiloTitulo.setFont(fuenteTitulo);
 	    
 	    // Fusionar celdas para el título
-	    hoja.addMergedRegion(new CellRangeAddress(1, 1, 1, 4)); // Fusionar B2:F2
+	    hoja.addMergedRegion(new CellRangeAddress(1, 1, 1, 12)); // Fusionar B2:F2
+	    
+	    estiloTitulo.setAlignment(HorizontalAlignment.CENTER); // Centrar horizontalmente el contenido
 	    
 	    Cell celdaTitulo = tituloFila.createCell(1);
-	    celdaTitulo.setCellValue("Tabla de Empleados");
+	    celdaTitulo.setCellValue("Lista de Empleados");
 	    celdaTitulo.setCellStyle(estiloTitulo);
 	}
 
@@ -81,31 +83,59 @@ public class EmpleadoExporterExcel
 	    celda.setCellStyle(estiloCabecera);
 	    
 	    celda = filaCabecera.createCell(2);
-	    celda.setCellValue("Puesto");
+	    celda.setCellValue("Nombre");
 	    celda.setCellStyle(estiloCabecera);
-	    
+
 	    celda = filaCabecera.createCell(3);
-	    celda.setCellValue("Fecha Contrato");
+	    celda.setCellValue("Apellido");
 	    celda.setCellStyle(estiloCabecera);
 	    
 	    celda = filaCabecera.createCell(4);
-	    celda.setCellValue("Horario Laboral");
+	    celda.setCellValue("DNI");
 	    celda.setCellStyle(estiloCabecera);
 	    
 	    celda = filaCabecera.createCell(5);
-	    celda.setCellValue("Salario");
+	    celda.setCellValue("Teléfono");
 	    celda.setCellStyle(estiloCabecera);
+	    
+	    celda = filaCabecera.createCell(6);
+	    celda.setCellValue("Correo");
+	    celda.setCellStyle(estiloCabecera);
+	    
+	    celda = filaCabecera.createCell(7);
+	    celda.setCellValue("Fecha Nacimiento");
+	    celda.setCellStyle(estiloCabecera);
+	    
+	    celda = filaCabecera.createCell(8);
+	    celda.setCellValue("Sexo");
+	    celda.setCellStyle(estiloCabecera);
+	    
+	    celda = filaCabecera.createCell(9);
+	    celda.setCellValue("Puesto");
+	    celda.setCellStyle(estiloCabecera);
+	    
+	    celda = filaCabecera.createCell(10);
+	    celda.setCellValue("Fecha Contrato");
+	    celda.setCellStyle(estiloCabecera);
+	    
+	    celda = filaCabecera.createCell(11);
+	    celda.setCellValue("Horario Laboral");
+	    celda.setCellStyle(estiloCabecera);
+	    
+	    celda = filaCabecera.createCell(12);
+	    celda.setCellValue("Salario");
+	    celda.setCellStyle(estiloCabecera);  
+	    
 	}
 
 	private void escribirDatosDeLaTabla() {
 	    int numeroFilas = 4; // Comenzar desde la fila 5 (B5)
-	    
+
 	    CellStyle estiloDatos = libro.createCellStyle();
 	    XSSFFont fuenteDatos = libro.createFont();
 	    fuenteDatos.setFontHeight(14);
 	    estiloDatos.setFont(fuenteDatos);
-	    
-	    // Definir bordes para las celdas de datos
+
 	    estiloDatos.setBorderBottom(BorderStyle.THIN);
 	    estiloDatos.setBottomBorderColor(IndexedColors.BLACK.getIndex());
 	    estiloDatos.setBorderTop(BorderStyle.THIN);
@@ -116,36 +146,65 @@ public class EmpleadoExporterExcel
 	    estiloDatos.setRightBorderColor(IndexedColors.BLACK.getIndex());
 
 	    for (Empleado empleado : listaEmpleados) {
+	        Usuario usuario = empleado.getUsuario(); // Obtener el usuario asociado al empleado
+
 	        Row fila = hoja.createRow(numeroFilas++);
+
+	        Cell celdaEmpleadoId = fila.createCell(1);
+	        celdaEmpleadoId.setCellValue(empleado.getId_Empleado());
+	        celdaEmpleadoId.setCellStyle(estiloDatos);
 	        
-	        for (int i = 1; i < 6; i++) { // Comenzar desde B hasta F (columnas 2 a 6)
-	            Cell celda = fila.createCell(i);
-	            celda.setCellStyle(estiloDatos);
-	            
-	            switch (i) {
-	                case 1:
-	                    celda.setCellValue(empleado.getId_Empleado());
-	                    break;
-	                case 2:
-	                    celda.setCellValue(empleado.getPuesto());
-	                    break;
-	                case 3:
-	                    celda.setCellValue(empleado.getFecha_Contratacion());
-	                    break;
-	                case 4:
-	                    celda.setCellValue(empleado.getHorario_Trabajo());
-	                    break;
-	                case 5:
-	                    celda.setCellValue(empleado.getSalario().toString());
-	                    break;
-	                default:
-	                    break;
-	            }
-	            
-	            hoja.autoSizeColumn(i); // Ajustar el ancho de la columna automáticamente
-	        }
+	        Cell celdaUsuarioNombres = fila.createCell(2);
+	        celdaUsuarioNombres.setCellValue(usuario.getNombres());
+	        celdaUsuarioNombres.setCellStyle(estiloDatos);
+	        
+	        Cell celdaUsuarioApellido = fila.createCell(3);
+	        celdaUsuarioApellido.setCellValue(usuario.getApellidos());
+	        celdaUsuarioApellido.setCellStyle(estiloDatos);
+	        
+	        Cell celdaUsuarioDNI = fila.createCell(4);
+	        celdaUsuarioDNI.setCellValue(usuario.getDni());
+	        celdaUsuarioDNI.setCellStyle(estiloDatos);
+	        
+	        Cell celdaUsuarioTelefono = fila.createCell(5);
+	        celdaUsuarioTelefono.setCellValue(usuario.getTelefono());
+	        celdaUsuarioTelefono.setCellStyle(estiloDatos);
+	        
+	        Cell celdaUsuarioCorreo = fila.createCell(6);
+	        celdaUsuarioCorreo.setCellValue(usuario.getCorreo());
+	        celdaUsuarioCorreo.setCellStyle(estiloDatos);
+	        
+	        Cell celdaUsuarioNacimiento = fila.createCell(7);
+	        celdaUsuarioNacimiento.setCellValue(usuario.getFecha_Nacimiento());
+	        celdaUsuarioNacimiento.setCellStyle(estiloDatos);
+	        
+	        Cell celdaUsuarioSexo = fila.createCell(8);
+	        celdaUsuarioSexo.setCellValue(usuario.getSexo());
+	        celdaUsuarioSexo.setCellStyle(estiloDatos);
+
+	        Cell celdaEmpleadoPuesto = fila.createCell(9);
+	        celdaEmpleadoPuesto.setCellValue(empleado.getPuesto());
+	        celdaEmpleadoPuesto.setCellStyle(estiloDatos);
+
+	        Cell celdaEmpleadoFechaContrato = fila.createCell(10);
+	        celdaEmpleadoFechaContrato.setCellValue(empleado.getFecha_Contratacion());
+	        celdaEmpleadoFechaContrato.setCellStyle(estiloDatos);
+
+	        Cell celdaEmpleadoHorario = fila.createCell(11);
+	        celdaEmpleadoHorario.setCellValue(empleado.getHorario_Trabajo());
+	        celdaEmpleadoHorario.setCellStyle(estiloDatos);
+
+	        Cell celdaEmpleadoSalario = fila.createCell(12);
+	        celdaEmpleadoSalario.setCellValue(empleado.getSalario().toString());
+	        celdaEmpleadoSalario.setCellStyle(estiloDatos);
+	    }
+
+	    // Autoajustar el ancho de las columnas después de insertar los datos
+	    for (int i = 1; i <= 12; i++) { // Comenzar desde la columna 1 hasta la 8
+	        hoja.autoSizeColumn(i);
 	    }
 	}
+
 	
 	public void exportar(HttpServletResponse response) throws IOException
 	{
